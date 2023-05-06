304c39b1c8b58f75de720504bb9c24eea1df22ac
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-05-06 17:11:41 +0530

    improve code

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import random
 import tkinter as tk
-from backend import Backend
+import backend
 
 
 class App(tk.Tk):
     def __init__(self):
-        super().__init__()
+        super().__init__()  # creates a Tk window
+
         self.gravity_enabled = False
-
         self.main_menu_screen = True
-        self.game_state = 'ready'
 
-        self.backend = Backend()
+        self.backend = backend.Backend()
+
+        self.canvas_bg_images : list[int] = []
+        self.canvas_pillar_images: list[list[int, int]] = []
+        self.current_score = 0
+
+        self.store_currently_used_assets()
         self.init_window()
-
-        self.update_used_assets()
-
-        self.background_images : list[int] = []
         self.init_background()
-        self.scroll_background()
-
         self.init_bird()
         self.init_buttons()
-
-
-        self.pillars_currently_visible = []
         self.init_pillars()
-
-        self.score = 0
         self.init_scoreboard()
 
         self.mainloop()
@@ -43,126 +37,233 @@ class App(tk.Tk):
         self.game_window_width = 600
         self.game_window_height = 500
 
-        pos_y = int((self.user_screen_width - self.game_window_width)/2)
-        pos_x = int((self.user_screen_height - self.game_window_height)/2)
+        pos_x = int((self.user_screen_width - self.game_window_width)/2)
+        pos_y = int((self.user_screen_height - self.game_window_height)/2)
 
-        self.geometry(f"{self.game_window_width}x{self.game_window_height}+{pos_y}+{pos_x}")
-        self.canvas = tk.Canvas(self, height=500, width=600)
+        self.geometry(f"{self.game_window_width}x{self.game_window_height}+{pos_x}+{pos_y}")
+        self.canvas = tk.Canvas(self, height=self.game_window_height, width=self.game_window_width)
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    def update_used_assets(self):
-        self.bg_img = self.backend.get_current_bg_image()
-        self.bird_img = self.backend.get_current_player_image()
+    def store_currently_used_assets(self):
+        self.background_image = self.backend.get_current_bg_image()
+        self.player_icon_image = self.backend.get_current_player_image()
         self.pillar_up_image, self.pillar_down_image = self.backend.get_current_pillar_images()
 
+        self.play_button_image, self.exit_button_image = self.backend.get_buttons_images()
+
     def init_background(self):
-        bg_img1 = self.canvas.create_image(0, 0, image=self.bg_img, anchor=tk.NW)
-        bg_img2 = self.canvas.create_image(self.game_window_width, 0, image=self.bg_img, anchor=tk.NW)
-        bg_img3 = self.canvas.create_image(self.game_window_width*2, 0, image=self.bg_img, anchor=tk.NW)
+        bg_img1 = self.canvas.create_image(0, 0, image=self.background_image, anchor=tk.NW)
+        bg_img2 = self.canvas.create_image(self.game_window_width, 0, image=self.background_image, anchor=tk.NW)
+        bg_img3 = self.canvas.create_image(self.game_window_width * 2, 0, image=self.background_image, anchor=tk.NW)
 
-        self.background_images = [bg_img1, bg_img2, bg_img3]
+        self.canvas_bg_images = [bg_img1, bg_img2, bg_img3]
         self.update()
-
-    def shift_unseen_background(self):
-        old_x_of_first_img = self.canvas.coords(self.background_images[0])[0]
-
-        if old_x_of_first_img < -self.game_window_width-2:
-            # print("shifted")
-            increment = 2*self.game_window_width - 5
-            self.canvas.move(self.background_images[0], increment, 0)
-
-            temp = self.background_images.pop(0)
-            self.background_images.append(temp)
+        self.scroll_background()
 
     def scroll_background(self):
-        for i, img in enumerate(self.background_images):
+        for i, img in enumerate(self.canvas_bg_images):
             self.canvas.move(img, -2, 0)
 
         self.shift_unseen_background()
         self.after(50, self.scroll_background)
 
+    def shift_unseen_background(self):
+        x_coord_of_first_img, _ = self.canvas.coords(self.canvas_bg_images[0])
+
+        if x_coord_of_first_img < -self.game_window_width-2:  # out of the screen
+            increment = 2*self.game_window_width - 5
+            # shift to the right side of the last background image
+
+            self.canvas.move(self.canvas_bg_images[0], increment, 0)
+
+            first_bg_img = self.canvas_bg_images.pop(0)
+            self.canvas_bg_images.append(first_bg_img)
+
+    # bird
     def init_bird(self):
-        bird_img = self.bird_img
-        self.bird_canvas_image = self.canvas.create_image(200, 200, image=bird_img)
+        bird_img = self.player_icon_image
+        self.bird_canvas_image = self.canvas.create_image(0, 0, image=bird_img)
         self.canvas.moveto(self.bird_canvas_image, 200, 200)
 
         self.bird_velocity = 0
-        self.gravity_acceleration = 0.6
+        self.terminal_velocity = 9.5  # maximum downwards velocity the bird can reach
+        self.gravity_acceleration = 0.6   # pixels per gravity_interval that will be added to the bird's velocity
         self.gravity_interval = 15
+        self.bottom_death_limit = 470  # coords below which the bird dies, for ground death
 
         self.bind("<Button-1>", lambda e: self.make_bird_jump())
-
-        self.make_bird_fall()
+        # left click event binded to the game window
 
     def make_bird_fall(self):
         if not self.gravity_enabled:
             return
 
         self.bird_velocity += self.gravity_acceleration
-        if self.bird_velocity > 9:
-            self.bird_velocity = 9
+        self.bird_velocity = min(self.bird_velocity, self.terminal_velocity)
 
         self.canvas.move(self.bird_canvas_image, 0, self.bird_velocity)
+        # moves the bird in y direction by the current velocity amount of pixels
 
-        y_coords = self.canvas.coords(self.bird_canvas_image)[1]
-        if y_coords >= 470:
-            self.game_over()
-
-        self.after(self.gravity_interval, self.make_bird_fall)
-
-    def make_bird_jump(self):
-        if self.main_menu_screen:  # user presses PLAY
+        _, y_coords = self.canvas.coords(self.bird_canvas_image)
+        if y_coords >= self.bottom_death_limit:
+            self.lose_game()
+            print("shift this block to another function later")
             return
 
-        if self.ready_to_go:
-            self.ready_to_go = False  # game starts
+        self.after(self.gravity_interval, self.make_bird_fall)  # recursive call to continuously simulate gravity
+
+    def make_bird_jump(self):
+        if self.main_menu_screen:
             return
 
         self.bird_velocity = -10.5
-        if not self.gravity_enabled:
-            self.enable_gravity()
-            self.start_spawning_pillars()
+        # bird provided an upward velocity
 
+    # main menu
     def init_buttons(self):
-        self.playbutton, self.exitbutton = self.backend.get_buttons_images()
+        self.play_button_canvas_image = self.canvas.create_image(
+            self.game_window_width/2, 290,
+            image=self.play_button_image
+        )
+        self.exit_button_canvas_image = self.canvas.create_image(
+            self.game_window_width/2, 370,
+            image=self.exit_button_image
+        )
 
-        self.playimage_button = self.canvas.create_image(300, 290, image=self.playbutton)
-        self.exitimage_button = self.canvas.create_image(300, 350, image=self.exitbutton)
-
-        self.canvas.tag_bind(self.playimage_button, '<Button-1>', lambda e: self.start_game())
-        self.canvas.tag_bind(self.exitimage_button, '<Button-1>', lambda e: self.end_game())
+        self.canvas.tag_bind(self.play_button_canvas_image, '<Button-1>', lambda e: self.new_game())
+        self.canvas.tag_bind(self.exit_button_canvas_image, '<ButtonRelease-1>', lambda e: self.exit_game())
 
     def hide_buttons(self):
-        self.canvas.itemconfigure(self.playimage_button, state='hidden')
-        self.canvas.itemconfigure(self.exitimage_button, state='hidden')
+        self.canvas.itemconfigure(self.play_button_canvas_image, state='hidden')
+        self.canvas.itemconfigure(self.exit_button_canvas_image, state='hidden')
 
     def show_buttons(self):
-        self.canvas.itemconfigure(self.playimage_button, state='normal')
-        self.canvas.itemconfigure(self.exitimage_button, state='normal')
-        self.canvas.lift(self.playimage_button)
-        self.canvas.lift(self.exitimage_button)
+        self.canvas.itemconfigure(self.play_button_canvas_image, state='normal')
+        self.canvas.itemconfigure(self.exit_button_canvas_image, state='normal')
+        self.canvas.lift(self.play_button_canvas_image)
+        self.canvas.lift(self.exit_button_canvas_image)
 
-    def game_over(self):
-        self.game_state = 'stopped'
+    # pillars and scores
+    def init_scoreboard(self):
+        self.scoreboard = self.canvas.create_text(300, 50, text=f"{self.current_score}", font=("Calibri", 48, "bold"), state='hidden')
+
+    def update_scoreboard(self):
+        self.canvas.itemconfigure(self.scoreboard, text=f'{self.current_score}')
+
+    def increment_score(self):
+        self.current_score += 1
+        self.update_scoreboard()
+
+    def reset_score(self):
+        self.current_score = 0
+        self.update_scoreboard()
+
+    def init_pillars(self):
+        self.pillar_spawnpoint_x = self.game_window_width + 100  # x coord of the first off-screen pillar in a new game
+        self.pillar_distance = 300  # distance between 2 pillars' top left corners
+        self.pillar_height = 400  # height of a pillar (image file)
+        self.pillar_hole_gap = 165  # distance between bottom edge of top pillar and top edge of bottom pillar
+
+        self.STANDARD_PILLAR_UP_Y = (self.game_window_height - self.pillar_hole_gap)/2 - self.pillar_height
+        self.STANDARD_PILLAR_DOWN_Y = self.STANDARD_PILLAR_UP_Y + self.pillar_height + self.pillar_hole_gap
+        # standard position of both the pillars, making the hole lie exactly in the center of the window height
+
+        for i in range(3):
+            pillar_up = self.canvas.create_image(0, 0, image=self.pillar_up_image)
+            pillar_down = self.canvas.create_image(0, 0, image=self.pillar_down_image)
+
+            self.canvas_pillar_images.append([pillar_up, pillar_down])
+
+        self.reset_pillars_to_inital_position()
+
+    def reset_pillars_to_inital_position(self):
+        for i, [pillar_up, pillar_down] in enumerate(self.canvas_pillar_images):
+            random_shift = random.randint(-10, 10) * 10
+
+            self.canvas.moveto(
+                pillar_up,
+                self.pillar_spawnpoint_x + (self.pillar_distance * i),
+                self.STANDARD_PILLAR_UP_Y + random_shift
+            )
+            self.canvas.moveto(
+                pillar_down,
+                self.pillar_spawnpoint_x + (self.pillar_distance * i),
+                self.STANDARD_PILLAR_DOWN_Y + random_shift
+            )
+
+    def shift_unseen_pillar(self):
+        first_pillar_up, first_pillar_down = self.canvas_pillar_images[0]
+
+        random_shift = random.randint(-10, 10) * 10
+
+        self.canvas.moveto(first_pillar_up, self.pillar_spawnpoint_x, self.STANDARD_PILLAR_UP_Y + random_shift)
+        self.canvas.moveto(first_pillar_down, self.pillar_spawnpoint_x, self.STANDARD_PILLAR_DOWN_Y + random_shift)
+
+        temp = self.canvas_pillar_images.pop(0)
+        self.canvas_pillar_images.append(temp)
+
+    def keep_moving_pillars(self):
+        if self.main_menu_screen:
+            return
+
+        for pillar_up, pillar_down in self.canvas_pillar_images:
+            self.canvas.move(pillar_up, -3, 0)
+            self.canvas.move(pillar_down, -3, 0)
+
+        self.after(10, self.keep_moving_pillars)
+
+    def keep_spawning_pillars(self):
+        if self.main_menu_screen:
+            return
+
+        last_pillar_up, last_pillar_down = self.canvas_pillar_images[-1]
+        xcoord_up, _ = self.canvas.coords(last_pillar_up)
+
+        if self.pillar_spawnpoint_x - xcoord_up >= self.pillar_distance - 50:
+            # if the first pillar goes out of visual range
+            print("shifted", self.canvas_pillar_images)
+            self.shift_unseen_pillar()
+
+        self.after(50, self.keep_spawning_pillars)
+
+    def check_pillar_for_score(self):
+        pilup, pildown = self.canvas_pillar_images[0]
+        pilup2, pildown2 = self.canvas_pillar_images[1]
+
+    # game states
+    def lose_game(self):
+        print("lose game")
         self.main_menu_screen = True
         self.disable_gravity()
-        self.after(200, self.show_buttons)
+        self.after(500, self.show_buttons)
         self.canvas.lift(self.scoreboard)
+        self.bind("<Button-1>", lambda e: print(end=""))
+
+    def new_game(self):
+        print("new game")
+        self.hide_buttons()
+        self.main_menu_screen = False
+        self.canvas.moveto(self.bird_canvas_image, 200, 200)
+        self.canvas.itemconfigure(self.scoreboard, state='normal')
+        self.reset_score()
+        self.reset_pillars_to_inital_position()
+        self.after(50, lambda: [self.bind("<Button-1>", lambda e: self.start_game())])
 
     def start_game(self):
-        self.hide_buttons()
-        self.canvas.moveto(self.bird_canvas_image, 200, 200)
-        self.ready_to_go = True
-        self.main_menu_screen = False
-        self.update_scoreboard(reset=True)
-        self.canvas.itemconfigure(self.scoreboard, state='normal')
-        self.despawn_all_pillars()
+        print("started")
+        self.enable_gravity()
+        self.keep_moving_pillars()
+        self.keep_spawning_pillars()
+        self.check_pillar_for_score()
+        self.make_bird_jump()
+        self.bind("<Button-1>", lambda e: self.make_bird_jump())
 
-    def end_game(self):
-        self.canvas.delete(self.scoreboard)
-        self.gravity_enabled = True
-        self.make_bird_fall()
-        self.after(800, self.destroy)
+    def exit_game(self):
+        self.canvas.delete(self.scoreboard)  # done to avoid a weird tkinter error when destroying game window
+        self.after(200, self.destroy)
+
+    def check_if_player_lost(self):
+        pass
 
     def disable_gravity(self):
         self.gravity_enabled = False
@@ -170,89 +271,6 @@ class App(tk.Tk):
     def enable_gravity(self):
         self.gravity_enabled = True
         self.make_bird_fall()
-
-    def init_scoreboard(self):
-        self.scoreboard = self.canvas.create_text(300, 50, text=f"{self.score}", font=("Calibri", 48, "bold"), state='hidden')
-
-    def update_scoreboard(self, reset=False):
-        self.score += 1
-        if reset:
-            self.score = 0
-
-        self.canvas.itemconfigure(self.scoreboard, text=f'{self.score}')
-
-    def init_pillars(self):
-        self.pillar_spawnpoint_x = 700
-        self.pillar_distance = 300
-        self.pillar_height = 400
-        self.pillar_hole_gap = 165
-
-        self.STANDARD_PILLAR_UP_Y = (self.game_window_height - self.pillar_hole_gap)/2 - self.pillar_height
-        self.STANDARD_PILLAR_DOWN_Y = self.STANDARD_PILLAR_UP_Y + self.pillar_height + self.pillar_hole_gap
-
-    def spawn_pillars(self):
-        for i in range(3):
-            pillar_up_canvas = self.canvas.create_image(-200, 0, image=self.pillar_up_image)
-            pillar_down_canvas = self.canvas.create_image(-200, 0, image=self.pillar_down_image)
-
-            random_shift = random.randint(-10, 10) * 10
-
-            self.canvas.moveto(pillar_up_canvas, self.pillar_spawnpoint_x + self.pillar_distance*i, self.STANDARD_PILLAR_UP_Y + random_shift)
-            self.canvas.moveto(pillar_down_canvas, self.pillar_spawnpoint_x + self.pillar_distance*i, self.STANDARD_PILLAR_DOWN_Y + random_shift)
-
-            self.pillars_currently_visible.append([pillar_up_canvas, pillar_down_canvas])
-
-    def shift_unseen_pillar(self):
-        first_pillar_up, first_pillar_down = self.pillars_currently_visible[0]
-        xcoord_up, _ = self.canvas.coords(first_pillar_up)
-
-        random_shift = random.randint(-10, 10) * 10
-
-        self.canvas.moveto(first_pillar_up, self.pillar_spawnpoint_x, self.STANDARD_PILLAR_UP_Y + random_shift)
-        self.canvas.moveto(first_pillar_down, self.pillar_spawnpoint_x, self.STANDARD_PILLAR_DOWN_Y + random_shift)
-        temp = self.pillars_currently_visible.pop(0)
-        self.pillars_currently_visible.append(temp)
-
-        # print("shifted", self.pillars_currently_visible)
-
-    def move_pillars(self):
-        if self.main_menu_screen or self.ready_to_go:
-            return
-
-        for pillar_up, pillar_down in self.pillars_currently_visible:
-            self.canvas.move(pillar_up, -3, 0)
-            self.canvas.move(pillar_down, -3, 0)
-
-        self.after(10, self.move_pillars)
-
-    def keep_spawning_pillars(self):
-        if self.main_menu_screen or self.ready_to_go:
-            return
-
-        last_pillar_up, last_pillar_down = self.pillars_currently_visible[-1]
-        xcoord_up, _ = self.canvas.coords(last_pillar_up)
-
-        if self.pillar_spawnpoint_x - xcoord_up >= self.pillar_distance - 50:
-            # print("spawned", self.pillars_currently_visible)
-            self.shift_unseen_pillar()
-
-        self.after(50, self.keep_spawning_pillars)
-
-    def start_spawning_pillars(self):
-        self.move_pillars()
-        self.keep_spawning_pillars()
-        self.check_pillar_for_score()
-
-    def despawn_all_pillars(self):
-        for pilup, pildown in self.pillars_currently_visible:
-            self.canvas.delete(pilup, pildown)
-
-        self.pillars_currently_visible = []
-        self.spawn_pillars()
-
-    def check_pillar_for_score(self):
-        pilup, pildown = self.pillars_currently_visible[0]
-        pilup2, pildown2 = self.pillars_currently_visible[1]
 
 
 if __name__ == '__main__':
